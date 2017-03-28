23c6d7dca8df7ee2a34dcc83fbcdbec450d81d17
Peter Schmiedeskamp <peter@thoughtspot.net>
2017-03-28 15:40:06 -0700

    set deployment targets

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject snujure "0.1.0-SNAPSHOT"
+(defproject org.clojars.pschmied/snujure "0.1.0-SNAPSHOT"
   :description "Minimalist wrapper for soda-java"
   :url "http://github.com/pschmied/snujure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.socrata/soda-api-java "0.9.12"]])
+                 [com.socrata/soda-api-java "0.9.12"]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]])
